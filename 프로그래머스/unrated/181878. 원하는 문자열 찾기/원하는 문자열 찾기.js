3bf99be876c48ca58701e7365e4f31e2be6e06f7
kijiwon <119961147+kijiwon@users.noreply.github.com>
2023-08-08 18:32:54 +0900

    [unrated] Title: 원하는 문자열 찾기, Time: 0.15 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
@@ -0,0 +1,8 @@
+function solution(myString, pat) {
+    let str1 = myString.toLowerCase();
+    let str2 = pat.toLowerCase();
+    if(str1.includes(str2)){
+        return 1;
+    }
+    return 0;
+}
