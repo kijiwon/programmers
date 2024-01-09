09b88ed7d7ef794e4fc666a450f316ba80972c42
kijiwon <119961147+kijiwon@users.noreply.github.com>
2024-01-09 23:03:16 +0900

    [level unrated] Title: 수열과 구간 쿼리 1, Time: 3.92 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
@@ -0,0 +1,11 @@
+function solution(arr, queries) {
+    var answer = [];
+    for(let i=0; i<arr.length; i++){
+        for(let j=0; j<queries.length; j++){
+            if(i>= queries[j][0] && i<= queries[j][1]){
+                arr[i]++
+            }
+        }
+    }
+    return arr;
+}
