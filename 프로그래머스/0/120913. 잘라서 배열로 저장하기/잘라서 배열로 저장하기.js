8b458f05c8fb71ee4d320e43ee58eb383de3cace
kijiwon <119961147+kijiwon@users.noreply.github.com>
2025-07-02 20:02:02 +0900

    Update 잘라서 배열로 저장하기.js

--- 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -1,3 +1,9 @@
 function solution(my_str, n) {
-    return my_str.match(new RegExp(`.{1,${n}}`,'g'));
+    // 정규표현식
+    // return my_str.match(new RegExp(`.{1,${n}}`,'g'));
+    let result = [];
+    for(let i=0; i<my_str.length; i+=n){ // n만큼 건너뛰기
+        result.push(my_str.slice(i, i+n));
+    }
+    return result;
 }
