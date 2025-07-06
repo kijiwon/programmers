095072377b048a61c421e66076bf6f6709347110
kijiwon <119961147+kijiwon@users.noreply.github.com>
2025-07-06 20:05:55 +0900

    [level 0] Title: 7의 개수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.js
@@ -0,0 +1,8 @@
+function solution(array) {
+    var answer = 0;
+    let str = array.join('');
+    for(let i=0; i<str.length; i++){
+        if(str[i] === '7') answer++;
+    }
+    return answer;
+}
