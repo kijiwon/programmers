31c0c39a9d79c1a40b128f27590a4a3abfaa0c32
kijiwon <119961147+kijiwon@users.noreply.github.com>
2026-05-06 19:17:50 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,9 @@
+function solution(num) {
+    let answer = 0;
+    if(num ===1) return 0;
+    while(num > 1){
+        num = num%2===0? num/2 : (num*3)+1;
+        answer++;
+    }
+    return answer>=500? -1:answer;
+}
